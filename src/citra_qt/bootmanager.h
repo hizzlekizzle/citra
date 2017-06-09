66884155d4e7ba6569d804784c32fd8b7c5e910f
James <jselby@jselby.net>
2017-06-10 01:14:44 +1000

    Fix Intel iGPUs (only clear framebuffer if required)

--- src/citra_qt/bootmanager.h
+++ src/citra_qt/bootmanager.h
@@ -180,6 +180,7 @@ public:
     void SetupFramebuffer() override;
 
     bool ShouldDeferRendererInit() const override;
+    bool NeedsClearing() const override;
 
     void OnClientAreaResized(unsigned width, unsigned height);
 
--- src/video_core/renderer_opengl/renderer_opengl.cpp
+++ src/video_core/renderer_opengl/renderer_opengl.cpp
@@ -442,6 +442,8 @@ void RendererOpenGL::RenderScreenshot() {
 }
 
 void RendererOpenGL::PrepareRendertarget() {
+    render_window->SetupFramebuffer();
+
     for (int i : {0, 1, 2}) {
         int fb_id = i == 2 ? 1 : 0;
         const auto& framebuffer = GPU::g_regs.framebuffer_config[fb_id];
@@ -964,7 +966,10 @@ void RendererOpenGL::DrawScreens(const Layout::FramebufferLayout& layout, bool f
     const auto& bottom_screen = layout.bottom_screen;
 
     glViewport(0, 0, layout.width, layout.height);
-    glClear(GL_COLOR_BUFFER_BIT);
+
+    if (render_window->NeedsClearing()) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
 
     // Set projection matrix
     std::array<GLfloat, 3 * 2> ortho_matrix =
